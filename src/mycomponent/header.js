e07fbff10cb78b4c995d0fca984d74c894568dd7
kapildev01 <kapil.dev.e21@gmail.com>
2021-07-19 14:35:26 +0530

    changes

--- src/mycomponent/header.js
+++ src/mycomponent/header.js
@@ -17,7 +17,7 @@ export default function Header(props) {
                             <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/about">About</Link>
+                            <Link className="nav-link" to="/about">Aout</Link>
                         </li>
                     </ul>
                     {props.search?<form className="d-flex">
